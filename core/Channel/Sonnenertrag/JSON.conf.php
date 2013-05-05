6e23e76948a07a5ccd4e7cbca80bf37c8b61de27
Knut Kohl <github@knutkohl.de>
2013-05-05 22:23:34 +0200

    Added JSON extractor for Sonnenertrag.eu

---
+++ core/Channel/Sonnenertrag/JSON.conf.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ *
+ *
+ * @author      Knut Kohl <github@knutkohl.de>
+ * @copyright   2012-2013 Knut Kohl
+ * @license     GNU General Public License http://www.gnu.org/licenses/gpl.txt
+ * @version     $Id$
+ */
+return array(
+
+	/**
+	 * Possible keys:
+	 * - visible			 : (TRUE|FALSE), default TRUE
+	 * - default			 : default value
+	 */
+	'serial' => array(
+		'visible' => FALSE,
+	),
+	'channel' => array(
+		'visible' => FALSE,
+	),
+	'resolution' => array(
+		'visible' => FALSE,
+	),
+	'unit' => array(
+		'visible' => FALSE,
+	),
+	'meter' => array(
+		'visible' => FALSE,
+	),
+	'numeric' => array(
+		'visible' => FALSE,
+	),
+	'cost' => array(
+		'visible' => FALSE,
+	),
+	'threshold' => array(
+		'visible' => FALSE,
+	),
+	'valid_from' => array(
+		'visible' => FALSE,
+	),
+	'valid_to' => array(
+		'visible' => FALSE,
+	)
+
+);
