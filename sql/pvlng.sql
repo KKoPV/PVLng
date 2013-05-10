36daf9af4c2ecf1daa3ac5ae8454872ee18d0937
Knut Kohl <github@knutkohl.de>
2013-05-10 21:34:24 +0200

    Missing SQL file in release

--- sql/pvlng.sql
+++ sql/pvlng.sql
@@ -934,7 +934,7 @@ REPLACE INTO `pvlng_babelkit` VALUES ("app", "de", "LogoutSuccessful", "[b]%s[/b
 REPLACE INTO `pvlng_babelkit` VALUES ("app", "en", "GoToVariants", "Go to your Variants", "0", "");
 REPLACE INTO `pvlng_babelkit` VALUES ("app", "en", "LogoutSuccessful", "[b]%s[/b] logged out successful.", "0", "");
 REPLACE INTO `pvlng_babelkit` VALUES ("app", "en", "Variants", "Variants", "0", "");
-REPLACE INTO `pvlng_type` VALUES ("47", "Sonnenertrag JSON", "Readout plant/inverter data for Sonnenertrag JSON import", "Sonnenertrag\JSON", "Wh", "-1", "1", "0", "0", "sonnenertrag.png");
+REPLACE INTO `pvlng_type` VALUES ("47", "Sonnenertrag JSON", "Readout plant/inverter data for Sonnenertrag JSON import", "Sonnenertrag\\JSON", "Wh", "-1", "1", "0", "0", "sonnenertrag.png");
 
 -- Show API key
 
