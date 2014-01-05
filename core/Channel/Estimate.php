1e7d9ef8c6f6566887dc1c13a865ac2f026cc099
Knut Kohl <github@knutkohl.de>
2014-01-05 22:46:29 +0100

    Change estimate calculation for given day based on month values

--- core/Channel/Estimate.php
+++ core/Channel/Estimate.php
@@ -38,14 +38,23 @@ class Estimate extends InternalCalc {
         $estimates = array();
         foreach ($data as $line) {
             $line = explode(':', $line, 2);
-            if (isset($line[1])) $estimates[trim($line[0])] = $line[1];
+            if (isset($line[1])) {
+                if (preg_match('~0?(\d+)-0?(\d+)~', $line[0], $parts)) {
+                    $estimates[$parts[1].'-'.$parts[2]] = $line[1];
+                } else {
+                    $estimates[trim($line[0])] = $line[1];
+                }
+            }
         }
+
+        if (isset($estimates[1])) $estimates[13] = $estimates[1];
+        if (isset($estimates[12])) $estimates[0] = $estimates[12];
 
         $values = array();
 
         while ($timestamp <= $this->end) {
-            $day   = date('m-d', $timestamp);
-            $month = date('m', $timestamp);
+            $day   = date('n-j', $timestamp);
+            $month = date('n', $timestamp);
 
             // Round between 16:00 and 21:00 during year in seconds
             $ts = (16 + sin(date('z', $timestamp) * M_PI / 366) * 5) *60*60;
@@ -58,8 +67,19 @@ class Estimate extends InternalCalc {
             // Search for estimate, 1st for exact day, then for month
             if (isset($estimates[$day])) {
                 $values[$ts] = $estimates[$day];
-            } elseif (isset($estimates[$month])) {
-                $values[$ts] = $estimates[$month];
+            } elseif (isset($estimates[$month-1], $estimates[$month], $estimates[$month+1])) {
+                if (date('j', $timestamp) < 15) {
+                    $e1 = $estimates[$month-1];
+                    $e2 = $estimates[$month];
+                    $d1 = strtotime('first day of last month', $timestamp) + 1209600; // 15 days
+                    $d2 = strtotime('first day of this month', $timestamp) + 1209600;
+                } else {
+                    $e1 = $estimates[$month];
+                    $e2 = $estimates[$month+1];
+                    $d1 = strtotime('first day of this month', $timestamp) + 1209600;
+                    $d2 = strtotime('first day of next month', $timestamp) + 1209600;
+                } //         start + (          0 ... 1         ) * delta
+                $values[$ts] = $e1 + ($timestamp-$d1) / ($d2-$d1) * ($e2-$e1);
             }
             $timestamp += 86400;
         }
