0daf7e1d711c5ae17a8b6426abd48ecb799cad3d
Knut Kohl <github@knutkohl.de>
2014-01-05 12:25:19 +0100

    Put template search paths into Controller::before() to allow manual rendering from within controller actions

--- frontend/Controller.php
+++ frontend/Controller.php
@@ -55,6 +55,19 @@ class Controller extends slimMVC\Controller {
     /**
      *
      */
+    public function before() {
+        $controller = str_replace('Controller\\', '', get_class($this));
+        $this->view->BaseDir = array(
+            APP_DIR . DS . 'View' . DS . $controller . DS . 'custom',
+            APP_DIR . DS . 'View' . DS . $controller,
+            APP_DIR . DS . 'View' . DS . 'custom',
+            APP_DIR . DS . 'View'
+        );
+    }
+
+    /**
+     *
+     */
     public function after() {
         /* For Logout */
         $this->view->User = $this->User;
@@ -98,14 +111,6 @@ class Controller extends slimMVC\Controller {
         $this->view->Title = $this->config['Title'];
 
         $controller = str_replace('Controller\\', '', get_class($this));
-
-        $this->view->BaseDir = array(
-            APP_DIR . DS . 'View' . DS . $controller . DS . 'custom',
-            APP_DIR . DS . 'View' . DS . $controller,
-            APP_DIR . DS . 'View' . DS . 'custom',
-            APP_DIR . DS . 'View'
-        );
-
 
         $messages = array();
         foreach (Messages::getRaw() as $message) {
