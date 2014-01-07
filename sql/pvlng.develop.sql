e52b65fee0dc9267043ae74ee598eb7da0f0693a
Knut Kohl <github@knutkohl.de>
2014-01-07 22:19:53 +0100

    Update SQL files

---
+++ sql/pvlng.develop.sql
@@ -0,0 +1,18 @@
+--
+-- For development branch only!
+--
+
+DROP PROCEDURE `getTimestamp`;
+
+DELIMITER ;;
+
+CREATE PROCEDURE `getTimestamp` (INOUT `timestamp` int unsigned)
+IF `timestamp` = 0 THEN
+  SET `timestamp` = UNIX_TIMESTAMP();
+END IF;;
+
+DELIMITER ;
+
+REPLACE INTO `pvlng_babelkit` (`code_set`, `code_lang`, `code_code`, `code_desc`) VALUES
+('model',	'de',	'Estimate_commentHint',	'Definiere die Erwartungswerte auf Monats- oder Tagesbasis.\r\n\r\nWenn nur Monatswerte zur Verfügung stehen (z.B. von [url=http://re.jrc.ec.europa.eu/pvgis/apps4/pvest.php]PVGIS[/url]) werden diese als Werte des 15. des Monats verwendet und die anderen Tageswerte linear interpoliert.\r\n[list][*]Monat: [font=courier]Monat:Wert[/font]\r\n[*]Tag: [font=courier]Monat-Tag:Wert[/font][/list]\r\nBeispiel für einen Januar, 4,5kWh pro Tag\r\n[list][*]Monat: [font=courier]1:4.5[/font]\r\n[*]Tag (1. Januar): [font=courier]01-01:4.5[/font][/list]'),
+('model',	'en',	'Estimate_commentHint',	'Define your estimates on monthly or daily base.\r\n\r\nIf only monthly data exists (e.g from [url=http://re.jrc.ec.europa.eu/pvgis/apps4/pvest.php]PVGIS[/url]) the values will be used as of the 15th of the month and the other values will be linear interpolated to get daily values.\r\n[list][*]Monthly: [font=courier]month:value[/font]\r\n[*]Daily: [font=courier]month-day:value[/font][/list]\r\nExample for a january, 4.5kWh each day\r\n[list][*]Monthly: [font=courier]1:4.5[/font]\r\n[*]Daily (1st of january): [font=courier]01-01:4.5[/font][/list]');
