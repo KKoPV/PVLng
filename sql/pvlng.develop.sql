d85d6c6adeab588f099f1b7d92d8514f64f57dae
Knut Kohl <github@knutkohl.de>
2014-01-25 12:56:08 +0100

    Update development SQL script, reqired for Extra data on channels

--- sql/pvlng.develop.sql
+++ sql/pvlng.develop.sql
@@ -4,6 +4,9 @@
 
 ALTER TABLE `pvlng_channel` ADD `extra` text NOT NULL COMMENT 'Not visible field for models to store extra info' AFTER `public`;
 
+CREATE OR REPLACE VIEW `pvlng_tree_view` AS
+select `n`.`id` AS `id`,`n`.`entity` AS `entity`,if(`t`.`childs`,`n`.`guid`,`c`.`guid`) AS `guid`,if(`co`.`id`,`co`.`name`,`c`.`name`) AS `name`,if(`co`.`id`,`co`.`serial`,`c`.`serial`) AS `serial`,`c`.`channel` AS `channel`,if(`co`.`id`,`co`.`description`,`c`.`description`) AS `description`,if(`co`.`id`,`co`.`resolution`,`c`.`resolution`) AS `resolution`,if(`co`.`id`,`co`.`cost`,`c`.`cost`) AS `cost`,if(`co`.`id`,`co`.`meter`,`c`.`meter`) AS `meter`,if(`co`.`id`,`co`.`numeric`,`c`.`numeric`) AS `numeric`,if(`co`.`id`,`co`.`offset`,`c`.`offset`) AS `offset`,if(`co`.`id`,`co`.`adjust`,`c`.`adjust`) AS `adjust`,if(`co`.`id`,`co`.`unit`,`c`.`unit`) AS `unit`,if(`co`.`id`,`co`.`decimals`,`c`.`decimals`) AS `decimals`,if(`co`.`id`,`co`.`threshold`,`c`.`threshold`) AS `threshold`,if(`co`.`id`,`co`.`valid_from`,`c`.`valid_from`) AS `valid_from`,if(`co`.`id`,`co`.`valid_to`,`c`.`valid_to`) AS `valid_to`,if(`co`.`id`,`co`.`public`,`c`.`public`) AS `public`,c.extra,`c`.`comment` AS `comment`,`t`.`name` AS `type`,`t`.`model` AS `model`,`t`.`childs` AS `childs`,`t`.`read` AS `read`,`t`.`write` AS `write`,`t`.`graph` AS `graph`,`t`.`icon` AS `icon`,`ca`.`id` AS `alias`,`ta`.`id` AS `alias_of`,(((count(0) - 1) + (`n`.`lft` > 1)) + 1) AS `level`,round((((`n`.`rgt` - `n`.`lft`) - 1) / 2),0) AS `haschilds`,((((min(`p`.`rgt`) - `n`.`rgt`) - (`n`.`lft` > 1)) / 2) > 0) AS `lower`,((`n`.`lft` - max(`p`.`lft`)) > 1) AS `upper` from ((((((`pvlng_tree` `n` join `pvlng_tree` `p`) join `pvlng_channel` `c` on((`n`.`entity` = `c`.`id`))) join `pvlng_type` `t` on((`c`.`type` = `t`.`id`))) left join `pvlng_channel` `ca` on(((if(`t`.`childs`,`n`.`guid`,`c`.`guid`) = `ca`.`channel`) and (`ca`.`type` = 0)))) left join `pvlng_tree` `ta` on((`c`.`channel` = `ta`.`guid`))) left join `pvlng_channel` `co` on(((`ta`.`entity` = `co`.`id`) and (`c`.`type` = 0)))) where ((`n`.`lft` between `p`.`lft` and `p`.`rgt`) and ((`p`.`id` <> `n`.`id`) or (`n`.`lft` = 1))) group by `n`.`id` order by `n`.`lft`;
+
 DROP TRIGGER `pvlng_reading_num_bi`;
 
 DELIMITER ;;
@@ -46,6 +49,37 @@ IF new.`timestamp` = 0 THEN
 
   SET new.`timestamp` = @NOW;
 
+END IF;;
+
+DELIMITER ;
+
+CREATE TABLE `pvlng_changes` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `table` enum('babelkit','channel','config','log','options','performance','performance_avg','reading_num','reading_str','tree','type','view') NOT NULL COMMENT 'Table name',
+  `key` varchar(50) NOT NULL COMMENT 'Primary key value(s), for composed keys separated by "::"',
+  `field` varchar(50) NOT NULL COMMENT 'Field name',
+  `timestamp` int(10) unsigned NOT NULL COMMENT 'When was changed',
+  `old` varchar(256) NOT NULL COMMENT 'Old value',
+  `new` varchar(256) NOT NULL COMMENT 'New value',
+  PRIMARY KEY (`id`),
+  KEY `table` (`table`,`key`,`field`,`timestamp`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DELIMITER ;;
+
+CREATE TRIGGER `pvlng_changes_bi` BEFORE INSERT ON `pvlng_changes` FOR EACH ROW
+SET new.`timestamp` = UNIX_TIMESTAMP();;
+
+CREATE TRIGGER `pvlng_channel_au` AFTER UPDATE ON `pvlng_channel` FOR EACH ROW
+BEGIN
+  IF new.`adjust` = 1 THEN
+     CALL `pvlng_changed`('channel', new.`id`, 'offset', old.`offset`, new.`offset`);
+  END IF;
+END;;
+
+CREATE PROCEDURE `pvlng_changed`(IN `in_table` varchar(50), IN `in_key` varchar(50), IN `in_field` varchar(50), IN `in_old` varchar(255), IN `in_new` varchar(255))
+IF in_old <> in_new THEN
+  INSERT INTO `pvlng_changes` (`table`, `key`, `field`, `old`, `new`) VALUES (in_table, in_key, in_field, in_old, in_new);
 END IF;;
 
 DELIMITER ;
