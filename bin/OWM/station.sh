a8b09cf303492b190f967765ced74ae1c9029e03
Knut Kohl <github@knutkohl.de>
2014-02-08 12:36:17 +0100

    Add OpenWeatherMap template and script for storing weather data

---
+++ bin/OWM/station.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+##############################################################################
+### @author      Knut Kohl <github@knutkohl.de>
+### @copyright   2012-2013 Knut Kohl
+### @license     GNU General Public License http://www.gnu.org/licenses/gpl.txt
+### @version     1.0.0
+##############################################################################
+
+##############################################################################
+### Init
+##############################################################################
+pwd=$(dirname $0)
+
+. $pwd/../PVLng.conf
+. $pwd/../PVLng.sh
+
+while getopts "tvxh" OPTION; do
+    case "$OPTION" in
+        t) TEST=y; VERBOSE=$((VERBOSE + 1)) ;;
+        v) VERBOSE=$((VERBOSE + 1)) ;;
+        x) TRACE=y ;;
+        h) usage; exit ;;
+        ?) usage; exit 1 ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+read_config "$1"
+
+##############################################################################
+### Start
+##############################################################################
+test "$TRACE" && set -x
+
+test "$LAT_LON" || error_exit "Missing loaction coordinates (LAT_LON)!"
+
+loc="$(echo "$LAT_LON" | sed -e 's~,~\t~')"
+set $loc
+LATITUDE=$1
+LONGITUDE=$2
+
+test "$LATITUDE" -a "$LONGITUDE" || error_exit "Invalid loaction definition (LOCATION)!"
+
+test "$GUID" || error_exit "Missing OpenWeatherMap channel GUID (GUID)!"
+
+##############################################################################
+### Go
+##############################################################################
+RESPONSEFILE=$(mktemp /tmp/pvlng.XXXXXX)
+
+trap 'rm -f $TMPFILE $RESPONSEFILE >/dev/null 2>&1' 0
+
+# http://api.openweathermap.org/data/2.5/weather?lat=51.54805&lon=12.13125&units=metric&APPID=edfbe28d77cd456ae014bbc030730ed
+API_URL="http://api.openweathermap.org/data/2.5/weather?lat=$LATITUDE&lon=$LONGITUDE&units=metric"
+
+test "$APPID" && API_URL="$API_URL&APPID=$APPID"
+test "$LANGUAGE" && API_URL="$API_URL&lang=$LANGUAGE"
+
+log 2 "$API_URL"
+
+curl="$(curl_cmd)"
+
+### Query OpenWeatherMap API
+$curl --output $RESPONSEFILE $API_URL
+rc=$?
+
+log 2 @$RESPONSEFILE
+
+if test $rc -ne 0; then
+     error_exit "cUrl error for OpenWeatherMap API: $rc"
+fi
+
+code=$($curl --request POST --header "Content-Type: application/json" \
+             --data-binary @$RESPONSEFILE $PVLngURL/json/cod.txt)
+
+if test "$code" != "200"; then
+    cat $RESPONSEFILE
+    exit $code
+fi
+
+### Test mode
+log 2 "OpenWeatherMap API response:"
+log 2 @$RESPONSEFILE
+
+test "$TEST" || PVLngPUT $GUID @$RESPONSEFILE
+
+exit
+
+##############################################################################
+# USAGE >>
+
+Fetch data from OpenWeatherMap API
+
+Usage: $scriptname [options] config_file
+
+Options:
+    -t   Test mode, don't post
+         Sets verbosity to info level
+    -v   Set verbosity level to info level
+    -vv  Set verbosity level to debug level
+    -h   Show this help
+
+See $pwd/station.conf.dist for details.
+
+# << USAGE
---
+++ core/Channel/Templates/OpenWeatherMap.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * @author      Knut Kohl <github@knutkohl.de>
+ * @copyright   2012-2013 Knut Kohl
+ * @license     GNU General Public License http://www.gnu.org/licenses/gpl.txt
+ * @version     1.0.0
+ *
+ * 1.0.0
+ * - Inital creation
+ */
+return array(
+
+    'name' => '<a href="http://openweathermap.org/" targe="_blank">OpenWeatherMap</a>',
+
+    'description' => '
+        Pre-configured sensors:
+        <ul>
+            <li>Temperature (°C)</li>
+            <li>Atmospheric pressure (hPa)</li>
+            <li>Humidity (%)</li>
+            <li>Wind speed (m/s)</li>
+            <li>Wind direction (°)</li>
+            <li>Clouds (%)</li>
+            <li>Condition</li>
+            <li>Condition code</li>
+            <li>Condition icon Id</li>
+        </ul>
+    ',
+
+    'channels' => array(
+
+        // Grouping channel
+        array(
+            'type'        => 45, // OpenWeatherMap
+            'name'        => 'OpenWeatherMap',
+            'description' => '<a href="http://openweathermap.org/" targe="_blank">OpenWeatherMap</a>',
+        ),
+
+        // Real channels
+        array(
+            'type'        => 60, // Temperature sensor
+            'name'        => 'Temperature',
+            'channel'     => 'main->temp',
+            'unit'        => '°C',
+            'decimals'    => 1,
+        ),
+
+        array(
+            'type'        => 58, // Pressure sensor
+            'name'        => 'Atmospheric pressure',
+            'channel'     => 'main->pressure',
+            'unit'        => 'hPa',
+            'decimals'    => 1,
+        ),
+
+        array(
+            'type'        => 56, // Humidity sensor
+            'name'        => 'Humidity',
+            'channel'     => 'main->humidity',
+            'unit'        => '%',
+            'decimals'    => 0,
+            'valid_from'  => 0,
+            'valid_to'    => 100,
+        ),
+
+        array(
+            'type'        => 63, // Windspeed sensor
+            'name'        => 'Wind speed',
+            'channel'     => 'wind->speed',
+            'unit'        => 'm/s',
+            'decimals'    => 1,
+        ),
+
+        array(
+            'type'        => 91, // Switch
+            'name'        => 'Wind direction',
+            'channel'     => 'wind->deg',
+            'unit'        => '°',
+            'decimals'    => 0,
+            'valid_from'  => 0,
+            'valid_to'    => 360,
+        ),
+
+        array(
+            'type'        => 91, // Switch
+            'name'        => 'Clouds',
+            'channel'     => 'clouds->all',
+            'unit'        => '%',
+            'decimals'    => 0,
+            'valid_from'  => 0,
+            'valid_to'    => 100,
+        ),
+
+        array(
+            'type'        => 91, // Switch
+            'name'        => 'Condition',
+            'channel'     => 'weather->0->description',
+            'numeric'     => 0,
+        ),
+
+        array(
+            'type'        => 91, // Switch
+            'name'        => 'Condition code',
+            'channel'     => 'weather->0->id',
+            'decimals'    => 0,
+        ),
+
+        array(
+            'type'        => 91, // Switch
+            'name'        => 'Condition icon Id',
+            'channel'     => 'weather->0->icon',
+            'numeric'     => 0,
+        ),
+    )
+);
