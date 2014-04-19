51c3a239e26b4a06e8a534d5c874dba294a9fbc4
Knut Kohl <github@knutkohl.de>
2014-04-19 16:00:25 +0200

    Add new model AccumulatorFull

---
+++ core/Channel/AccumulatorFull.php
@@ -0,0 +1,125 @@
+<?php
+/**
+ * An Accumulator sums channels with the same unit to retrieve them as one channel
+ *
+ * @author      Knut Kohl <github@knutkohl.de>
+ * @copyright   2012-2013 Knut Kohl
+ * @license     GNU General Public License http://www.gnu.org/licenses/gpl.txt
+ * @version     1.0.0
+ */
+namespace Channel;
+
+/**
+ *
+ */
+class AccumulatorFull extends \Channel {
+
+    /**
+     * Channel type
+     * UNDEFINED_CHANNEL - concrete channel decides
+     * NUMERIC_CHANNEL   - concrete channel decides if sensor or meter
+     * SENSOR_CHANNEL    - numeric
+     * METER_CHANNEL     - numeric
+     */
+    const TYPE = NUMERIC_CHANNEL;
+
+    /**
+     * Accept only childs of the same entity type
+     */
+    public function addChild( $guid ) {
+        $childs = $this->getChilds();
+        if (empty($childs)) {
+            // Add 1st child
+            return parent::addChild($guid);
+        }
+
+        // Check if the new child have the same type as the 1st (and any other) child
+        $first = self::byID($childs[0]['entity']);
+        $new     = self::byGUID($guid);
+        if ($first->type == $new->type) {
+            // ok, add new child
+            return parent::addChild($guid);
+        }
+
+        throw new Exception('"'.$this->name.'" accepts only childs of type "'.$first->type.'"', 400);
+    }
+
+    /**
+     *
+     */
+    public function read( $request ) {
+
+        $this->before_read($request);
+
+        $childs = $this->getChilds();
+        $childCnt = count($childs);
+
+        switch($childCnt) {
+
+            case 0: // No childs, return empty result
+                $result = new \Buffer;
+                break;
+
+            case 1: // Only one child, return as is
+                $result = $childs[0]->read($request);
+                break;
+
+            default:
+                $buffer = $childs[0]->read($request);
+
+                // Combine all data for same timestamp
+                for ($i=1; $i<$childCnt; $i++) {
+
+                    $next = $childs[$i]->read($request);
+
+                    $row1 = $buffer->rewind()->current();
+                    $row2 = $next->rewind()->current();
+
+                    $result = new \Buffer;
+
+                    while (!empty($row1) OR !empty($row2)) {
+
+                        $key1 = $buffer->key();
+                        $key2 = $next->key();
+
+                        if ($key1 === $key2) {
+
+                            // same timestamp, combine
+                            $row1['data']        += $row2['data'];
+                            $row1['min']         += $row2['min'];
+                            $row1['max']         += $row2['max'];
+                            $row1['consumption'] += $row2['consumption'];
+
+                            $result->write($row1, $key1);
+                            $last = $row1['data'];
+
+                            // read both next rows
+                            $row1 = $buffer->next()->current();
+                            $row2 = $next->next()->current();
+
+                        } elseif (is_null($key2) OR !is_null($key1) AND $key1 < $key2) {
+
+                            $result->write($row1, $key1);
+
+                            // read only row 1
+                            $row1 = $buffer->next()->current();
+
+                        } else /* $key1 > $key2 */ {
+
+                            $result->write($row2, $key2);
+
+                            // read only row 2
+                            $row2 = $next->next()->current();
+
+                        }
+                    }
+                    $next->close();
+
+                    // Set result to buffer for next loop
+                    $buffer = $result;
+                }
+        } // switch
+
+        return $this->after_read($result);
+    }
+}
--- sql/pvlng.develop.sql
+++ sql/pvlng.develop.sql
@@ -2,7 +2,16 @@
 -- For development branch only!
 --
 
-INSERT INTO `pvlng_type`
-(`id`, `name`, `description`, `model`, `unit`, `childs`, `read`, `write`, `graph`, `icon`)
-VALUES
-(26, 'Meter to sensor', 'model::MeterToSensor', 'MeterToSensor', '', 1, 1, 0, 1, '/images/ico/calculator_scientific.png');
+ALTER TABLE `pvlng_type` ADD INDEX `childs` (`childs`), ADD INDEX `read` (`read`), ADD INDEX `write` (`write`);
+
+INSERT INTO `pvlng_type` (`id`, `name`, `description`, `model`, `unit`, `childs`, `read`, `write`, `graph`, `icon`) VALUES
+(26, 'Meter to sensor', 'model::MeterToSensor', 'MeterToSensor', '', 1, 1, 0, 1, '/images/ico/calculator_scientific.png'),
+(27, 'Full Accumulator', 'model::AccumulatorFull', 'AccumulatorFull', '', -1, 1, 0, 1, '/images/ico/calculator_scientific.png');
+
+REPLACE INTO `pvlng_babelkit` (`code_set`, `code_lang`, `code_code`, `code_desc`, `code_order`) VALUES
+('model', 'de', 'Accumulator', 'Summiert die Messwerte aller Sub-Kanäle für den gleichen Zeitpunkt und ignoriert alle Datensätze, wo mindestens ein Wert pro Zeitpunkt fehlt.', 0),
+('model', 'en', 'Accumulator', 'Build the sum of readings of all child channels for same timestamp and ignores data sets, where at least one for a timestamp ist missing.', 0),
+('model', 'de', 'AccumulatorFull', 'Summiert die Messwerte aller Sub-Kanäle für den gleichen Zeitpunkt, summiert die Werte auch, wenn ein Wert für einen Zeitpunkt fehlt.', 0),
+('model', 'en', 'AccumulatorFull', 'Build the sum of readings of all child channels for same timestamp, works for all timestamps, also if one data set is missing.', 0),
+('app', 'de', 'ClearSearch', 'Suchbegriff löschen', 0),
+('app', 'en', 'ClearSearch', 'Clear search term', 0);
