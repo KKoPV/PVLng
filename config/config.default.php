eb9ed0c9dc12f2c8f62eee33cf4829fea179aa6a
Knut Kohl <github@knutkohl.de>
2014-05-29 16:09:22 +0200

    Adjust configuration handling

--- config/config.default.php
+++ config/config.default.php
@@ -10,18 +10,32 @@
 return array(
 
     /**
-     *
+     * 1. Change the database credentials for your needs
      */
-    'Title' => 'PhotoVoltaic Logger new generation',
+    'Database' => array(
+        'Host'     => '<HOST>',
+        'Socket'   => '<SOCKET>',
+        'Port'     => '<PORT>',
+        'Username' => '<USER>',
+        'Password' => '<PASSWORD>',
+        'Database' => '<DATABASE>'
+    ),
 
     /**
-     * Currency settings
+     * 2. Protect your installation.
+     *
+     * Call http://your.domain.here/adminpass
+     * to setup your administrator account
      */
-    'Currency' => array(
-        'ISO'      => 'EUR',
-        'Symbol'   => '€',
-        'Decimals' => 2
+    'Admin' => array(
+        'User'     => NULL,
+        'Password' => NULL
     ),
+
+    /**
+     * Allow admin to login by IP bound token
+     */
+    'TokenLogin' => TRUE,
 
     /**
      * Loaction for sunrise/sunset
@@ -34,6 +48,21 @@ return array(
     'Location' => array(
         'Latitude'  => NULL,
         'Longitude' => NULL
+    ),
+
+    /**
+     *  Your personal title :-)
+     */
+    'Title' => 'PhotoVoltaic Logger new generation',
+
+    /**
+     * Currency settings
+     */
+    'Currency' => array(
+        'ISO'      => 'EUR',
+        'Symbol'   => '€',
+        'Decimals' => 2,
+        'Format'   => '%.2f €'
     ),
 
     /**
@@ -56,11 +85,24 @@ return array(
      * Preferd cache mode, auto detect best one if empty
      */
     'Cache' => NULL,
+    // Force one, if you like:
+    # 'Cache' => 'APC',          // Save each key
+    # 'Cache' => 'MemCache',     // Save each key
+    # 'Cache' => 'MemCacheOne',  // Save all data into one key (like file cache)
+    # 'Cache' => 'File',
+    # 'Cache' => 'Mock',         // Disable caching complete
 
     /**
      * Model flags
      */
     'Model' => array(
+
+        'Daylight' => array(
+            /**
+             * Average of last ? days
+             */
+            'CurveDays' => 5,
+        ),
 
         /**
          * How to calculate the average of data in History channels
@@ -72,6 +114,66 @@ return array(
          'History' => array(
              'Average' => 0
          ),
+
+         'Estimate' => array(
+             'Marker' => '/images/energy.png'
+         ),
+    ),
+
+    /**
+     * Controller settings
+     */
+    'Controller' => array(
+
+        'Index' => array(
+            /**
+             * Chart canvas height, width is 940px,
+             *
+             * - Ratio  5 x  4 : 752
+             * - Ratio  4 x  3 : 705
+             * - Ratio 16 x 10 : 587
+             * - Ratio 10 x  6 : 564
+             * - Ratio 16 x  9 : 528
+             */
+            'ChartHeight' => 528,
+
+            /**
+             * Chart auto refresh timeout in sec., set 0 for no automatic refresh
+             */
+            'Refresh' => 300,
+
+            /**
+             * Show notification for each loaded channel
+             */
+            'NotifyLoadEach' => FALSE,
+
+            /**
+             * Show notification for loaded channels when all channels was loaded
+             */
+            'NotifyLoadAll' => TRUE,
+        ),
+
+        'Dashboard' => array(
+            /**
+             * Login for dashboards required?
+             */
+            'Login' => TRUE,
+        ),
+
+        'Tariff' => array(
+            /**
+             * Tariff price line count
+             */
+            'TimesLineCount' => 6,
+        ),
+
+        'Mobile' => array(
+            /**
+             * Chart canvas height
+             */
+            'ChartHeight' => 320,
+        ),
+
     ),
 
     /**
@@ -79,24 +181,17 @@ return array(
      */
     'View' => array(
 
-        // Compile templates and reuse unless changed
-        'ReuseCode'        => TRUE,
-
-        // Replace static images by base64 encoded inline code
-        // Only for images with max x/y of 8px
+        /**
+         * Replace static images by base64 encoded inline code
+         * Only for images with max x/y of 8px
+         */
         'InlineImages'     => 8,
 
-        // Don't compress HTML
-        'Verbose'          => FALSE,
-
-        // FALSE, 0, 10, 62, (95 is not working with UTF-8)
+        /**
+         * FALSE, 0, 10, 62
+         */
         'JavaScriptPacker' => 62,
 
-        // XML View settings
-        'XML' => array(
-            'Node' => 'node',
-            'Data' => 'data',
-        ),
     ),
 
 );
