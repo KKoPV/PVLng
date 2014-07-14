95cb1bb1dec19340a91a1ba0f5ea3b413d8544d7
Knut Kohl <github@knutkohl.de>
2014-07-14 09:54:56 +0200

    Add new models for Average line and Kostal Piko inverters

---
+++ core/Channel/Averageline.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ *
+ *
+ * @author      Knut Kohl <github@knutkohl.de>
+ * @copyright   2012-2013 Knut Kohl
+ * @license     GNU General Public License http://www.gnu.org/licenses/gpl.txt
+ * @version     1.0.0
+ *
+ * 1.0.0
+ * - initial creation
+ */
+namespace Channel;
+
+/**
+ *
+ */
+class Averageline extends InternalCalc {
+
+    /**
+     *
+     */
+    public function before_read( $request ) {
+
+        parent::before_read($request);
+
+        $ts = $cnt = $sum = 0;
+
+        /**
+         * Calulated with the Hölder mean fomulas
+         * http://en.wikipedia.org/wiki/H%C3%B6lder_mean
+         */
+        $p = $this->extra;
+
+        foreach ($this->getChild(1)->read($request) as $row) {
+            if (!$ts) $ts = $row['timestamp'];
+            $sum += pow($row['data'], $p);
+            $cnt++;
+        }
+
+        if ($cnt) {
+            $avg = pow($sum / $cnt, 1/$p);
+
+            foreach ($this->getChild(1)->read($request) as $row) {
+                $this->saveValue($row['timestamp'], $avg);
+            }
+        }
+    }
+}
---
+++ core/Channel/Kostal/Piko.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ *
+ *
+ * @author      Knut Kohl <github@knutkohl.de>
+ * @copyright   2012-2014 Knut Kohl
+ * @license     MIT License (MIT) http://opensource.org/licenses/MIT
+ * @version     1.0.0
+ */
+namespace Channel\Kostal;
+
+/**
+ *
+ */
+class Piko extends \Channel {
+
+    /**
+     *
+     */
+    public function write( $request, $timestamp=NULL ) {
+
+        // DON'T call before_write() here, will be called inside each
+        // child->write() for each real reding value
+
+        // Split delivered file into lines
+        $data = explode("\n", $request);
+
+        if (count($data) < 2) return 0;
+
+        // Get channel names from 1st row
+        $names = array_shift($data);
+        // Split into array ...
+        $names = explode("\t", $names);
+        // ... and trim values ...
+        $names = array_map('trim', $names);
+        // ... and flip to set the names as keys
+        $names = array_flip($names);
+
+        $data = explode("\t", $data[0]);
+        $data = array_map('trim', $data);
+
+        $ok = 0;
+
+        // find valid child channels
+        foreach ($this->getChilds() as $child) {
+            if (!$child->write OR $child->channel == '') continue;
+
+            // Channel name fornd in data?
+            if (!isset($names[$child->channel])) continue;
+            $id = $names[$child->channel];
+
+            // Channel value found in data?
+            if (!isset($data[$id])) continue;
+            $value = $data[$id];
+
+            // Interpret empty numeric value as invalid
+            if ($child->numeric AND $value == '') continue;
+
+            try { // Simulate $request['data'], timestamp is in row[0]
+                $ok += $child->write(array('data' => $value), $data[0]);
+            } catch (\Exception $e) {
+                $code = $e->getCode();
+                if ($code != 200 AND $code != 201 AND $code != 422) throw $e;
+            }
+        }
+
+        return $ok;
+    }
+}
