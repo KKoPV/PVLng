14f220f2d26ee810a2090d445c4ada65f2c19058
Knut Kohl <github@knutkohl.de>
2014-08-12 20:52:43 +0200

    Add Infoframe

---
+++ frontend/Application/Infoframe.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ *
+ * @author     Knut Kohl <github@knutkohl.de>
+ * @copyright  2012-2014 Knut Kohl
+ * @license    MIT License (MIT) http://opensource.org/licenses/MIT
+ * @version    1.0.0
+ */
+
+/**
+ *
+ */
+PVLng::SubMenu(
+    'index', 10, '/infoframe', __('Infoframe')
+);
+
+/**
+ * Routes
+ */
+$app->get('/infoframe', function() use ($app) {
+    $app->process('Infoframe', 'Index', array('frame' => 'default'));
+});
+
+/**
+ * Custom frames
+ */
+$app->get('/infoframe/:frame', function($frame) use ($app) {
+    $app->process('Infoframe', 'Index', array('frame' => 'custom'.DS.$frame));
+});
