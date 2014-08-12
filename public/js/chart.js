90a03b6ffd92d2d4f800a7ce98d9bb6ebb333146
Knut Kohl <github@knutkohl.de>
2014-08-12 20:53:24 +0200

    Update scripts and styles

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,16 +1,16 @@
 /**
  *
  */
-var defaults = {
+var presentation_defaults = {
     /* Chart hint decimals */
     HintDecimals: 2,
 
     line: {
-        /* line type */
+        /* Line type */
         type: 'spline',
-        /* line width */
+        /* Line width */
         width: 2,
-        /* line color */
+        /* Line color */
         color: '#404040'
     }
 };
@@ -22,19 +22,19 @@ function presentation( data ) {
     /* Set defaults */
     this.v = 2;
     this.axis = 1;
-    this.type = defaults.line.type;
-    this.consumption = false;
+    this.type = presentation_defaults.line.type;
     this.style = 'Solid';
-    this.width = defaults.line.width;
-    this.color = defaults.line.color;
+    this.width = presentation_defaults.line.width;
+    this.color = presentation_defaults.line.color;
     /* Removed in v2
     this.coloruseneg = false;
-    this.colorneg = defaults.line.color;
+    this.colorneg = presentation_defaults.line.color;
     */
     /* Added in v2 */
     this.colorusediff = 0;
-    this.colordiff = defaults.line.color;
+    this.colordiff = presentation_defaults.line.color;
     /* --- */
+    this.consumption = false;
     this.threshold = 0;
     this.min = false;
     this.max = false;
@@ -48,7 +48,7 @@ function presentation( data ) {
     try {
         data = JSON.parse(data);
         if (typeof data.v == 'undefined') {
-            /* prior v3.0.0 */
+            /* prior v2 */
             if (data.coloruseneg === '1') {
                 data.colorusediff = -1;
             } else if (data.coloruseneg === '0') {
@@ -66,61 +66,61 @@ function presentation( data ) {
 
 /**
  *
- */
+ * /
 function setExtremes() {
 
     var i, e, extremes=[], p, pos=[], min=100, max=-100;
 
     for (i=0; i<chart.yAxis.length; i++) {
 
-        /* Reset extremes */
+        /* Reset extremes * /
         chart.yAxis[i].setExtremes(null, null);
 
-        /* Get extremes */
+        /* Get extremes * /
         e = chart.yAxis[i].getExtremes();
 
-        /* Calc rel. position of 0 value; 0 - top, 1 - bottom*/
+        /* Calc rel. position of 0 value; 0 - top, 1 - bottom * /
         p = e.max / (e.max-e.min);
 
-        /* Remember min/max positions */
+        /* Remember min/max positions * /
         min = Math.min(min, p);
         max = Math.max(max, p);
 
-        /* Remember extremes and positions */
+        /* Remember extremes and positions * /
         extremes.push( { min: e.min, max: e.max, height: e.max-e.min } );
         pos.push(p);
     }
 
-    /* Average to align to */
+    /* Average to align to * /
     var center = (max+min)/2;
 
     for (i=0; i<chart.yAxis.length; i++) {
 
         if (max <= 1) {
-            /* With neg. values */
+            /* With neg. values * /
             if (pos[i] < center) {
-                /* Add offset on top */
+                /* Add offset on top * /
                 extremes[i].max += (center-pos[i])*2 * extremes[i].height;
             } else {
-                /* Add offset at bottom */
+                /* Add offset at bottom * /
                 extremes[i].min -= (pos[i]-center)*2 * extremes[i].height;
             }
         } else if (min >= 1) {
-            /* Add offset at bottom */
+            /* Add offset at bottom * /
             extremes[i].min -= (pos[i]-min) * extremes[i].height;
         } else {
-            /* ??? */
+            /* ??? * /
         }
 
         extremes[i].min = Math.round(extremes[i].min * 100) / 100;
         extremes[i].max = Math.round(extremes[i].max * 100) / 100;
 
-        /* Add 5% min padding only for "real" neg. data */
+        /* Add 5% min padding only for "real" neg. data * /
         if (extremes[i].min < -0.001) {
             extremes[i].min -= (extremes[i].max-extremes[i].min) * 0.05;
         }
 
-        /* Add 5% max padding only for "real" pos. data */
+        /* Add 5% max padding only for "real" pos. data * /
         if (extremes[i].max > 0.001) {
             extremes[i].max += (extremes[i].max-extremes[i].min) * 0.05;
         }
@@ -136,19 +136,22 @@ function setExtremes() {
  */
 function setMinMax( serie, channel ) {
 
-    var
-        ts  = { min: Number.MAX_VALUE, max: -Number.MAX_VALUE },
+    if (serie.data.length == 0) return serie;
+
+    var ts  = { min: serie.data[0].x, max: serie.data[serie.data.length-1].x },
         min = { id: null, x: null, y:  Number.MAX_VALUE },
         max = { id: null, x: null, y: -Number.MAX_VALUE },
-        /* Use negativeColor as "original" color if serie shows data above threshold different */
+        /* Use negativeColor as "original" color if serie shows data ABOVE threshold different */
         color = (serie.colorDiff == 1) ? serie.negativeColor : serie.color;
 
-    /* search min. and max. values */
+    /* Search min. and max. values */
     $.each(serie.data, function(i, point) {
-        ts.min = Math.min(ts.min, point.x);
-        if (channel.min && (point.y < min.y)) min = { id: i, x: point.x, y: point.y };
-        ts.max = Math.max(ts.max, point.x);
-        if (channel.max && (point.y > max.y)) max = { id: i, x: point.x, y: point.y };
+        if (channel.min && (point.y < min.y)) {
+            min = { id: i, x: point.x, y: point.y };
+        }
+        if (channel.max && (point.y > max.y)) {
+            max = { id: i, x: point.x, y: point.y };
+        }
     });
 
     if (min.id != null) {
@@ -204,7 +207,7 @@ function setMinMax( serie, channel ) {
     var last = serie.data.length-1;
 
     /* Only if not still marked as min or max */
-    if (channel.last && (last != max.id) && (last != min.id) && (last >= 0)) {
+    if (channel.last && (last != max.id) && (last != min.id)) {
 
         serie.data[last].marker = {
             enabled: true,
